dc7af5b6d6dc9dd92f0ffca1919ba4ded1b59bdf
razij7 <razijamilf@gmail.com>
2016-12-16 18:47:47 +0700

    Gambar ok

---
+++ ChangeLogs.md
@@ -0,0 +1,39 @@
+# Dokumentasi riwayat perubahan
+\
+## Inisiasi instal 10/31/2016
+- 10312016001 Buat repositori
+- 10312016002 Buat file changeLogs
+- 10312016003 Instal dengan menggunakan phnegap
+- 10312016004 Mengubah direktori
+
+## POST using JQuery , XML 6/12/2016
+
+- GET schema
+	- tipe data bisa apa aja, kalo disini xml
+- Isi schema
+	- tipe data bisa apa aja, kalo disini xml trus di convert ke text
+- POST schema
+	- tipe data harus text
+	
+## controller untuk POST form carts, 7/12/2016 12:05
+
+## Update, Image, Form, dll 13/12/2016 16:24
+- index.html
+	- product image udah bisa kebaca dengan size kecil, yg skrg dipake "small_default", tipe2nya ada di ../images/products
+	- untuk ganti tipenya, tinggal ditambahin nama tipe tsb di akhir link
+	- contoh :
+		- var setUrlImageProduts = "http://ARMMR8JEW5FLK7AS63FLHUU72I29QDXP@go-obat.com/markets/api/images/products/"+apiIdProduct+
+			"/"+apiIdImage+"/small_default";
+		
+- post.js
+	- fungsi "postData" diganti jadi "create"
+	- tambahan fungsi "readId"
+		return data berupa file xml
+	- tambahan fungsi "updateId"
+	- untuk sementara masih dalam bentuk fungsi2, saya masih mempelajari OOPnya dulu
+	
+- Note:
+	- http://bootsnipp.com/forms
+		- website form builder, biar cepet bikin formnya, bikin form di web tsb, kemudian htmlnya bisa diambil lalu dicopas ke kodingan,
+			tinggal sinkronisasi dengan jquerynya
+	
---
+++ www/js/form.js
@@ -0,0 +1,194 @@
+var is_guest = '';
+var note = '';
+var max_payment_days = '';
+var id_risk = '';
+var show_public_prices = '';
+var outstanding_allow_amount = '';
+var ape = '';
+var siret = '';
+var company = '';
+var website = '';
+var optin = '';
+var newsletter = '';
+var birthday = '';
+var id_gender = '';
+var email = '';
+var lastname = '';
+var firstname = '';
+var passwd = '';
+var deleted = '';
+var last_passwd_gen = '';
+var ip_registration_newsletter = '';
+var newsletter_date_add = '';
+var id_default_group = '';
+
+var id_address_delivery = '';
+var id_address_invoice = '';
+var id_currency = '';
+var id_customer = '';
+var id_guest = ''; //tiap buka web, id guest nambah, kalo baru sign up, id customer masuk ke id guest,
+var id_lang = '';
+var id_shop_group = '';
+var id_shop = '';
+var id_carrier = '';
+var recyclable = '';
+var gift = '';
+var gift_message = '';
+var mobile_theme = '';
+var delivery_option = '';
+var secure_key = '';
+var allow_separated_package = '';
+var date_add = '';
+var date_upd = '';
+var id_product = '';
+var id_product_attribute = '';
+var quantity = '';
+var id_cart = '';
+var current_state = '';
+var module = '';
+var invoice_number = '';
+var invoice_date = '';
+var delivery_number = '';
+var delivery_date = '';
+var valid = '';
+var date_add = '';
+var date_upd = '';
+var shipping_number = '';
+var id_shop_group = '';
+var id_shop = '';
+var secure_key = '';
+var payment = '';
+var recyclable = '';
+var gift = '';
+var gift_message = '';
+var mobile_theme = '';
+var total_discounts = '';
+var total_discounts_tax_incl = '';
+var total_discounts_tax_excl = '';
+var total_paid = '';
+var total_paid_tax_incl = '';
+var total_paid_tax_excl = '';
+var total_paid_real = '';
+var total_products = '';
+var total_products_wt = '';
+var total_shipping = '';
+var total_shipping_tax_incl = '';
+var total_shipping_tax_excl = '';
+var carrier_tax_rate = '';
+var total_wrapping = '';
+var total_wrapping_tax_incl = '';
+var total_wrapping_tax_excl = '';
+var round_mode = '';
+var round_type = '';
+var conversion_rate = '';
+var reference = '';
+var product_id = '';
+var product_attribute_id = '';
+var product_quantity = '';
+var product_name = '';
+var product_reference = '';
+var product_ean13 = '';
+var product_upc = '';
+var product_price = '';
+var unit_price_tax_incl = '';
+var unit_price_tax_excl = '';
+var name = '';
+var active = '';
+var id = '';
+var id_manufacturer = '';
+var id_supplier = '';
+var id_warehouse = '';
+var id_country = '';
+var id_state = '';
+var alias = '';
+var vat_number = '';
+var address1 = '';
+var address2 = '';
+var postcode = '';
+var city = '';
+var other = '';
+var phone = '';
+var phone_mobile = '';
+var dni = '';
+var stats_date_from = '';
+var stats_date_to = '';
+var stats_compare_from = '';
+var stats_compare_to = '';
+var id_profile = '';
+var bo_color = '';
+var default_tab = '';
+var bo_theme = '';
+var bo_css = '';
+var bo_width = '';
+var bo_menu = '';
+var stats_compare_option = '';
+var preselect_date_range = '';
+var id_last_order = '';
+var id_last_customer_message = '';
+var id_last_customer = '';
+
+/*orders
+id_address_delivery = 55;
+id_address_invoice = 55;
+id_cart = 185;
+id_currency = 2;
+id_lang = 2;
+id_customer = 59;
+id_carrier = 3;
+current_state = 16;
+module = 'bca';
+payment = 'BCA';
+total_paid = 208000;
+total_paid_real = 208000;
+total_products = 1;
+total_products_wt = 1;
+conversion_rate = '';
+product_id = 139;
+product_quantity = 1;
+*/
+
+/*carts
+id_address_delivery = 55;
+id_address_invoice = 55;
+id_currency = 2;
+id_customer = 59;
+id_lang = 2;
+id_product = 139;
+quantity = 1;
+//id_guests = 0 karena id guest tergantung dari visit user ke web, 0 karena customer blm daftar
+*/
+
+
+
+//customers
+
+//id_default_group = 3;
+//id_lang = 2;
+//passwd = 'tesaja';
+//lastname = 'Aja';
+//firstname = 'Razi';
+//email = 'razijamil@improva.co.id';
+
+
+
+/*addresses -- lastname dan firstname harus di get dari customer sesuai id customer
+id_customer = 59;
+id_country = 111;
+id_state = 11;
+alias = 'Home';
+company = 'PT Improva';
+lastname = 'jamil';
+firstname = 'raziz';
+address1 = 'Pulomas Utara no 42';
+postcode = 13210;
+city = 'Jakarta Timur';
+phone = 109109;
+*/
+
+//employees
+id_lang = 2; 
+passwd = 'asdasd';
+lastname = 'J';
+firstname = 'R';
+email = 'razijamilf@gmail.com'
+id_profile = 3;
